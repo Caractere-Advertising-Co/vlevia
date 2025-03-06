b2a7a7415116b3fd534d3bd6606faadfbf7c6dbd
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-06 16:31:22 +0100

    simple-content

--- simple-content.php
+++ simple-content.php
@@ -3,28 +3,12 @@
 Template Name: Simple-content
 */
 
-get_header();
+get_header();?>
 
-$bg_header = get_field('bg_header');
-
-if(!$bg_header):
-    $bg_url = get_template_directory_uri(  ).'/assets/img/bg-default.jpg';
-else :
-    $bg_header = get_field('bg_header');
-    $bg_url = $bg_header['url'];
-endif;
-
-?>
-
-<header id="header-simple-page" >
-    <img src="<?php echo $bg_url;?>" alt="<?php echo $bg_header['title'];?>"/>
-
-    <div class="container">
-        <div class="content">
-            <h1><?php the_title();?></h1>
-        </div>
-    </div>
+<header id="header-simple-page">
+    <?php get_template_part('templates-parts/separator/separator-svg');?>
 </header>
+
 
 <section id="simple-content">
     <div class="container">
