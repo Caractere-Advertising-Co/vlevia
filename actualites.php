c6d35a11dd5256a22ed86c380b592a4969d475cb
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-06 15:31:54 +0100

    header actualites

--- actualites.php
+++ actualites.php
@@ -8,30 +8,20 @@ get_header();
 $surtitre = get_field('surtitre');
 $titre = get_field('titre');
 
-$bg_header = get_field('bg_header');
-
-if(!$bg_header):
-    $bg_url = get_template_directory_uri(  ).'/assets/img/bg-default.jpg';
-else :
-    $bg_header = get_field('bg_header');
-    $bg_url = $bg_header['url'];
-endif;
-
 $titreDec = get_field('decouverte');
 $cta = get_field('cta');
 
 ?>
 
-<header id="header-simple-page" >
-    <img src="<?php echo $bg_url;?>" alt="<?php echo $bg_header['title'];?>"/>
-
+<header id="header">
     <div class="container">
         <div class="content">
-            <span class="subtitle"><?php if($surtitre): echo $surtitre;endif;?></span>
             <?php if($titre): echo $titre; endif;?>
         </div>
     </div>
 </header>
+
+<?php get_template_part( 'templates-parts/separator/separator-svg' );?>
 
 <section id="simple-page">
     <div class="container">
