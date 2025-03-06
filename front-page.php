ece4fd7b831490e41c5ab45e4f36cb64469dbeb8
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-06 09:25:05 +0100

    $m

--- front-page.php
+++ front-page.php
@@ -95,7 +95,7 @@ $galerie        = get_field('galerie-menu');
     <div class="container columns menu-list">
         <?php if($menu):?>
             <?php foreach($menu as $m):?>
-                <a href="<?php echo get_permalink($m->ID);?>"><?php echo $m->post_title;?></a>
+                <a href="<?php echo $m['url'];?>"><?php echo $m['title'];?></a>
             <?php endforeach;
         endif;?>
     </div>
