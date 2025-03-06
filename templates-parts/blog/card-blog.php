2db9d58104672d8f024196e14ca0ad8bcf1b3ea5
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-06 15:10:42 +0100

    </p>

--- templates-parts/blog/card-blog.php
+++ templates-parts/blog/card-blog.php
@@ -13,7 +13,7 @@ $intro = get_field('texte_introduction');
 
     <div class="txt">
         <?php if($titre): echo $titre; endif;?>
-        <?php if($intro): echo strlen($intro) > 100 ? substr($intro,0,100).'...' : $intro ; endif;?>
+        <?php if($intro): echo strlen($intro) > 100 ? substr($intro,0,100).'...</p>' : $intro ; endif;?>
 
         <a href="<?php echo the_permalink( );?>" class="cta-border">Lire</a>
     </div>
